e77e8c0947a5a0a095520c2f066b0ffa8aa53f75
taylor <123@34itcast>
2019-10-27 19:32:13 +0800

    年份你翻翻

---
+++ git/src/cn/itcast/HelloGit.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+        System.out.println("你好 , git ............");
+    }
+}
